2aafad67dcb682cf7f84f3bfdae57847888644e2
Matheus Marinho <100873422+mt-marinho@users.noreply.github.com>
2023-02-02 23:02:41 -0300

    regdfg
    dfgdfg

--- src/curso/PrimeiraClasse.java
+++ src/curso/PrimeiraClasse.java
@@ -4,4 +4,7 @@ public class PrimeiraClasse {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+	public static void ei(String[] args) {
+		System.out.println("Hello world");
+	}
 }
